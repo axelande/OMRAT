6463cc09deada8947166b483d7fff780029f9ee5
Axel Hörteborn <axel.horteborn@sspa.se>
2024-02-21 11:20:33 +0100

    added pandas to assert almost

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -2,6 +2,8 @@ import json
 import os
 import sys
 
+import pandas as pd
+from pandas.testing import assert_series_equal
 import pytest
 from qgis.PyQt.QtWidgets import QApplication
 
@@ -17,7 +19,7 @@ def load_data(omrat:OpenMRAT):
 
 def test_run_calculation(load_data:OpenMRAT):
     load_data.dockwidget.pbRunModel.click()
-    exp_drift = {'tot_sum': 0.0006511175081618417, 
+    exp_drift = pd.Series({'tot_sum': 0.0006511175081618417, 
                  'l': {'1': {'lin_sum': 0.0006511175081618417, 
                              'North going': 0.0003163098044510794, 
                              'South going': 0.0003348077037107623}}, 
@@ -26,8 +28,8 @@ def test_run_calculation(load_data:OpenMRAT):
                  'all': {'1 - North going': {'Structure - 1': 9.535177957577136e-05, 
                                              'Depth - 1': 0.00022095802487530805}, 
                          '1 - South going': {'Structure - 1': 9.536139988973841e-05, 
-                                             'Depth - 1': 0.0002394463038210239}}}
-    exp_power = {'tot_sum': 0.0823688084188536, 
+                                             'Depth - 1': 0.0002394463038210239}}})
+    exp_power = pd.Series({'tot_sum': 0.0823688084188536, 
                  'l': {'1': {'lin_sum': 0.0823688084188536, 
                              'North going': 0.0823688084188536, 
                              'South going': 0}}, 
@@ -35,9 +37,9 @@ def test_run_calculation(load_data:OpenMRAT):
                        'Depth - 1': 0}, 
                  'all': {'1 - North going': {'Structure - 1': 0.0823688084188536, 
                                              'Depth - 1': 0}, 
-                         '1 - South going': {'Structure - 1': 0, 'Depth - 1': 0}}}
-    assert load_data.calc.drift_dict == exp_drift
-    assert load_data.calc.powered_dict == exp_power
+                         '1 - South going': {'Structure - 1': 0, 'Depth - 1': 0}}})
+    assert_series_equal(pd.Series(load_data.calc.drift_dict), exp_drift, check_exact=False)
+    assert_series_equal(pd.Series(load_data.calc.powered_dict), exp_power, check_exact=False)
 
 
 if __name__ == '__main__':
